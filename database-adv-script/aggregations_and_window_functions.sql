c092efdb10c43bb21872ee78b2504f34a614cd6a
Bashu-011 <bashu2609@gmail.com>
2025-09-05 08:54:26 +0300

    Switched to ROW_NUMBER for ordering

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -11,14 +11,13 @@ GROUP BY u.user_id, u.first_name, u.last_name
 ORDER BY total_bookings DESC;
 
 -- returns properties basend on the number of times they have been booked
-SELECT
+SELECT 
     p.property_id,
     p.name AS property_name,
-    p.location,
-    COUNT(b.bookimg_id) AS total_bookings,
-    RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank
+    COUNT(b.booking_id) AS total_bookings,
+    ROW_NUMBER() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank
 FROM Property p
 LEFT JOIN Booking b
     ON p.property_id = b.property_id
 GROUP BY p.property_id, p.name
-ORDER BY booking_rank
+ORDER BY booking_rank;
